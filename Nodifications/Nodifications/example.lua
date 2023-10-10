05472e6943ba5885f525d51c39c5748a599bc98b
FadeScripting <79726377+Fadewasheres@users.noreply.github.com>
2023-10-10 16:13:15 +0200

    Create example.lua

---
+++ Nodifications/Nodifications/example.lua
@@ -0,0 +1,11 @@
+local NotificationLoad = loadstring(game:HttpGet(('https://raw.githubusercontent.com/fadewasheres/fade-uis/Nodifications/source.lua'),true))()
+
+NotificationLoad:NewNotification({ 
+    ["Mode"] = "info", -- Choose one (Success/Info/Error)
+    ["Title"] = "World!", -- Title of notification
+    ["Description"] = "Hello World!", -- Description of notification
+    ["Timeout"] = 5, -- How long the notification will last
+    ["Audio"] = true, -- Plays audio if enabled on each notification
+    ["Credits"] = false -- just keep it false :D (UNLESS) u wanna support us it will print "Created By Fade" in F9
+    
+})
